7ed917c012fcffec1ebb3893100748075574f073
omar7100 <omar.s.alshoul@gmail.com>
2019-11-13 09:19:53 +0200

    ch2

--- code-challenges/value-vs-reference/challenges-02.test.js
+++ code-challenges/value-vs-reference/challenges-02.test.js
@@ -18,7 +18,7 @@ console.log(a) prints [1, 2, 3, 1]
 const appendFirstToLast = (arr) => {
     arr.push(arr[0]);
   };
-/* ------------------------------------------------------------------------------------------------
+/* ---------------------------------------------------------------------------------------------------
 CHALLENGE 3
 Write a function that accepts an object and an integer as arguments and adds a new property to the object called yearBorn. The value of the yearBorn property should be the integer that was passed in.
 The change should be reflected in the source object that was passed in to the function. That is, the function should modify the object 'in place'.
